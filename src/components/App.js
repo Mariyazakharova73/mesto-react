7a462dcca89e0b452ea8131c7db8855c5a8aa660
Mariya Zakharova <zakharovamaria73@yandex.ru>
2022-08-18 12:54:15 +0400

    open popup

--- src/components/App.js
+++ src/components/App.js
@@ -11,14 +11,14 @@ function App() {
         <Header />
         <Main />
         <Footer />
-        {/* <div className="popup popup_place_edit-button">
+        <div className="popup popup_place_edit-button">
           <div className="popup__content">
             <button className="popup__close" type="button"></button>
             <div className="popup__form-content">
               <h2 className="popup__form-heading">Редактировать профиль</h2>
               <form className="popup__form" name="editForm" novalidate>
                 <input id="name-input" className="popup__form-input" type="text" name="name" placeholder="Имя" minlength="2" maxlength="40" required />
-                <span class="name-input-error popup__input-error"></span>
+                <span className="name-input-error popup__input-error"></span>
                 <input id="job-input" className="popup__form-input" type="text" name="about" placeholder="О себе" minlength="2" maxlength="200" required />
                 <span className="job-input-error popup__input-error"></span>
                 <button className="popup__form-button" type="submit">
@@ -33,7 +33,7 @@ function App() {
             <button className="popup__close" type="button"></button>
             <div className="popup__form-content">
               <h2 className="popup__form-heading">Новое место</h2>
-              <form classclassName="popup__form" name="addForm" novalidate>
+              <form className="popup__form" name="addForm" novalidate>
                 <input id="title-input" className="popup__form-input" type="text" name="name" placeholder="Название" minlength="2" maxlength="30" required />
                 <span className="title-input-error popup__input-error"></span>
                 <input id="link-input" className="popup__form-input" type="url" name="link" placeholder="Ссылка на картинку" required />
@@ -50,16 +50,16 @@ function App() {
             <button className="popup__close" type="button"></button>
             <figure className="popup__image-container">
               <img className="popup__image" src="#" />
-              <figcaption class="popup__image-title"></figcaption>
+              <figcaption className="popup__image-title"></figcaption>
             </figure>
           </div>
         </div>
         <div className="popup popup_place_delete-button">
           <div className="popup__content">
-            <button class="popup__close" type="button"></button>
+            <button className="popup__close" type="button"></button>
             <div className="popup__form-content">
               <h2 className="popup__form-heading">Вы уверены?</h2>
-              <form clasNames="popup__form" name="deleteCard" novalidate>
+              <form className="popup__form" name="deleteCard" novalidate>
                 <button className="popup__form-button" type="submit">
                   Да
                 </button>
@@ -81,8 +81,21 @@ function App() {
               </form>
             </div>
           </div>
-        </div> */}
+        </div>
       </div>
+      <template id="card" className="card-template">
+        <li className="gallery__card">
+          <button className="gallery__button-delete gallery__button-delete_hidden" type="button"></button>
+          <img className="gallery__card-image" />
+          <div className="gallery__card-description">
+            <h2 className="gallery__card-heading"></h2>
+            <div className="button-like__container">
+              <button className="button-like" type="submit"></button>
+              <p className="button-like__number"></p>
+            </div>
+          </div>
+        </li>
+      </template>
     </div>
   );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,20 +1,38 @@
 import avatar from './../images/profile-avatar.jpg';
 
+function handleEditAvatarClick() {
+  const popupAvatar = document.querySelector('.popup_place_avatar');
+  popupAvatar.classList.add('popup_opened');
+  console.log('аватар');
+}
+
+function handleEditProfileClick() {
+  const popupProfile = document.querySelector('.popup_place_edit-button');
+  popupProfile.classList.add('popup_opened');
+  console.log('профиль');
+}
+
+function handleAddPlaceClick() {
+  const popupCard = document.querySelector('.popup_place_add-button');
+  popupCard.classList.add('popup_opened');
+  console.log('добавить карточку');
+}
+
 function Main() {
   return (
     <main className="content">
     <section className="profile">
-      <div className="profile__avatar-wrapper">
+      <div className="profile__avatar-wrapper" onClick={handleEditAvatarClick}>
         <img className="profile__avatar" src={avatar} alt="Аватар" />
       </div>
       <div className="profile__info">
         <div className="profile__info-wrapper">
           <h1 className="profile__info-name">Мария Захарова</h1>
-          <button className="profile__edit-button" type="button"></button>
+          <button className="profile__edit-button" type="button" onClick={handleEditProfileClick}></button>
         </div>
         <p className="profile__info-job">Студент</p>
       </div>
-      <button className="profile__add-buttton" type="button"></button>
+      <button className="profile__add-buttton" type="button" onClick={handleAddPlaceClick}></button>
     </section>
     <section className="gallery" aria-label="Галерея">
       <ul className="gallery__cards"></ul>
