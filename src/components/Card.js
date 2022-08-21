86bff5dc66b8cc834e5cc793c5dacd575f8ff494
Mariya Zakharova <zakharovamaria73@yandex.ru>
2022-08-21 13:30:07 +0400

    review

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,16 @@
-function Card({cardItem, onCardClick }) {
+function Card({ cardItem, onCardClick }) {
   function handleClick() {
     onCardClick(cardItem);
   }
 
   return (
     <li className="gallery__card">
-      <button className="gallery__button-delete" type="button"/>
+      <button className="gallery__button-delete" type="button" />
       <img className="gallery__card-image" src={cardItem.link} alt={`${cardItem.name}.`} onClick={handleClick} />
       <div className="gallery__card-description">
         <h2 className="gallery__card-heading">{cardItem.name}</h2>
         <div className="button-like__container">
-          <button className="button-like" type="button"/>
+          <button className="button-like" type="button" />
           <p className="button-like__number">{cardItem.likes.length}</p>
         </div>
       </div>
--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -2,7 +2,7 @@ function PopupWithForm({ isOpen, onClose, name, title, buttonText, children }) {
   return (
     <div className={`popup popup_place_${name} ${isOpen ? 'popup_opened' : ''}`}>
       <div className="popup__content">
-        <button className="popup__close" type="button" onClick={onClose}/>
+        <button className="popup__close" type="button" onClick={onClose} />
         <div className="popup__form-content">
           <h2 className="popup__form-heading">{title}</h2>
           <form className="popup__form" name={name}>
